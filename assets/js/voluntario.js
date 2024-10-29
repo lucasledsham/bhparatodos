86e6290f231ad249ced2d10bb77b12c6f2000737
Lucas Ledsham <lucasnunesleal@gmail.com>
2024-10-28 23:31:06 -0300

    Botao de buscar

--- assets/js/voluntario.js
+++ assets/js/voluntario.js
@@ -29,3 +29,42 @@ function adicionarEventosParaSecao(secao) {
 document.querySelectorAll('.carrossel section').forEach(section => {
   adicionarEventosParaSecao(section.id);
 });
+
+//BUSCA
+
+const searchInput = document.getElementById('searchInput');
+const tabs = document.querySelectorAll('.radio'); // Seleciona todos os inputs das abas
+
+searchInput.addEventListener('focus', function() {
+    document.getElementById('dois').checked = true; // Marca a opção A-Z
+});
+
+searchInput.addEventListener('input', function() {
+    const filter = this.value.toLowerCase();
+    const cards = document.querySelectorAll('.card-full');
+
+    cards.forEach(card => {
+        const title = card.querySelector('h3').textContent.toLowerCase();
+        if (title.includes(filter)) {
+            card.style.display = ''; // Mostra o card
+        } else {
+            card.style.display = 'none'; // Esconde o card
+        }
+    });
+});
+
+// Evento para redefinir busca ao mudar de aba
+tabs.forEach(tab => {
+    tab.addEventListener('change', function() {
+        // Limpa o campo de busca
+        searchInput.value = '';
+        const cards = document.querySelectorAll('.card-full');
+        
+        // Mostra todos os cards novamente
+        cards.forEach(card => {
+            card.style.display = ''; // Exibe todos os cards
+        });
+    });
+});
+
+
